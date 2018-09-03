335a3869172082e9975b7446776d50533ecb387b
joker <1586596499@qq.com>
2018-09-03 17:23:21 +0800

    cors

--- src/main/java/com/paopao/config/CORSConfiguration.java
+++ src/main/java/com/paopao/config/CORSConfiguration.java
@@ -15,7 +15,7 @@ public class CORSConfiguration {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
         // 设置你要允许的网站域名，如果全允许则设为 *
-        config.addAllowedOrigin("https://www.bestpaopao.cn");
+        config.addAllowedOrigin("https://bestpaopao.cn");
         // 如果要限制 HEADER 或 METHOD 请自行更改
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
